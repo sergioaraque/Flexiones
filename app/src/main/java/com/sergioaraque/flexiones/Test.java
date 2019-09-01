4f6f2a59fc5e06fb0519c3db62745d7f1065754f
Sergio Araque <sergio.araque97@gmail.com>
2019-09-01 22:17:21 +0200

    Contador de flexiones en test

--- app/src/main/java/com/sergioaraque/flexiones/Test.java
+++ app/src/main/java/com/sergioaraque/flexiones/Test.java
@@ -6,10 +6,14 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class Test extends AppCompatActivity {
 
     LinearLayout linearButton;
+    TextView tvContador;
+
+    int contador = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,11 +21,13 @@ public class Test extends AppCompatActivity {
 
 
         linearButton = (LinearLayout) findViewById (R.id.linearButton);
+        tvContador = (TextView) findViewById(R.id.tvContador);
 
+        tvContador.setText(contador + " flexiones");
     }
 
     public void linearButtonAction (View v) {
-        new AlertDialog.Builder(Test.this)
+        /*new AlertDialog.Builder(Test.this)
                 .setTitle("Your Alert")
                 .setMessage("Your Message")
                 .setCancelable(false)
@@ -30,6 +36,9 @@ public class Test extends AppCompatActivity {
                     public void onClick(DialogInterface dialog, int which) {
                         // Whatever...
                     }
-                }).show();
+                }).show();*/
+
+        contador += 1;
+        tvContador.setText(contador + " flexiones");
     };
 }
