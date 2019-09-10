05e881b4f3ceae3b45433b0d652915c760f9fec3
Sergio Araque <sergio.araque97@gmail.com>
2019-09-10 18:05:33 +0200

    Añadido el cronometro

--- app/src/main/java/com/sergioaraque/flexiones/Flexiones.java
+++ app/src/main/java/com/sergioaraque/flexiones/Flexiones.java
@@ -3,6 +3,7 @@ package com.sergioaraque.flexiones;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Chronometer;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -22,6 +23,10 @@ public class Flexiones extends AppCompatActivity {
         tvContador = (TextView) findViewById(R.id.tvContador);
 
         tvContador.setText(contador + " flexiones");
+
+        Chronometer simpleChronometer = (Chronometer) findViewById(R.id.simpleChronometer); // initiate a chronometer
+        simpleChronometer.start(); // start a chronometer
+        simpleChronometer.setFormat("Tiempo activo - %s"); // set the format for a chronometer
     }
 
     public void linearButtonAction (View v) {
