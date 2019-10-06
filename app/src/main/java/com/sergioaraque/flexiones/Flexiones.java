07a3163cf61263cfb32b351d5082227260c2309f
Sergio Araque <sergio.araque97@gmail.com>
2019-10-06 20:04:24 +0200

    Añadida la opción de parar el cronómetro y se han aumentado los tamaños de los textos en la ventana de flexiones

--- app/src/main/java/com/sergioaraque/flexiones/Flexiones.java
+++ app/src/main/java/com/sergioaraque/flexiones/Flexiones.java
@@ -3,6 +3,7 @@ package com.sergioaraque.flexiones;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Chronometer;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -11,31 +12,39 @@ public class Flexiones extends AppCompatActivity {
 
     LinearLayout linearLayoutContador;
     TextView tvContador;
+    Chronometer simpleChronometer;
+    Button btnParar;
 
     int contador = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.flexiones);
 
 
+        btnParar = (Button) findViewById(R.id.btnParar);
         linearLayoutContador = (LinearLayout) findViewById (R.id.linearLayoutContador);
         tvContador = (TextView) findViewById(R.id.tvContador);
 
         tvContador.setText(contador + " flexiones");
 
-        Chronometer simpleChronometer = (Chronometer) findViewById(R.id.simpleChronometer); // initiate a chronometer
+        simpleChronometer = (Chronometer) findViewById(R.id.simpleChronometer); // initiate a chronometer
         simpleChronometer.start(); // start a chronometer
         simpleChronometer.setFormat("Tiempo activo - %s"); // set the format for a chronometer
     }
 
     public void linearButtonAction (View v) {
-
         contador += 1;
         tvContador.setText(contador + " flexiones");
     }
 
     public void volverInicio(View v) {
         finish();
+    }
+
+    public void pararCronometro(View v) {
+        simpleChronometer.stop();
+        btnParar.setVisibility(View.INVISIBLE);
     }
 }
